e51c10e94d285433147cff8a0a2ef68f334041b2
Alec Jacob Deitloff <alec.deitloff@gmail.com>
2019-03-29 00:20:20 +0900

    switched projects over and set it up for jyosuushi

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default class App extends React.PureComponent {
+  public render() {
+    return <div>we're an app now with hot reloading</div>;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const ROOT_DIRECTORY = __dirname;
+const BUILD_DIRECTORY = path.resolve(ROOT_DIRECTORY, "build");
+const SOURCE_DIRECTORY = path.resolve(ROOT_DIRECTORY, "src");
+
+module.exports = {
+  entry: {
+    app: [SOURCE_DIRECTORY]
+  },
+  output: {
+    path: BUILD_DIRECTORY,
+    filename: "js/[name].bundle.js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(SOURCE_DIRECTORY, "index.html")
+    })
+  ]
+};
