fca3166d0cc244cac71fbaf75fd98bb0ce3092e8
Alec Jacob Deitloff <alec.deitloff@gmail.com>
2019-03-29 10:46:32 +0900

    reset CSS

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 
+import "meyer-reset-scss/reset.scss";
+
 ReactDOM.render(<App />, document.getElementById("root"));
