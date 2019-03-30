beac83febddbf260e630fbe75bfa1feb7324ae47
Alec Deitloff <alec.deitloff@gmail.com>
2019-03-30 16:52:21 -0700

    first pass at implementing conjugation of japanese numbers (no rendaku)

--- src/japanese/numbers.ts
+++ src/japanese/numbers.ts
@@ -1,4 +1,7 @@
 /* tslint:disable object-literal-sort-keys */
+import { memoize, uniqBy } from "lodash";
+import { permutate } from "../utils";
+
 const OKU = Math.pow(10, 8);
 const MAN = 10000;
 const SEN = 1000;
@@ -43,9 +46,191 @@ function breakDownNumber(value: number): NumberBreakdown {
 
 export interface JapaneseNumber {
   kana: string;
-  kanji: string;
+  kanji: string | null;
 }
 
-export function conjugate(amount: number): ReadonlyArray<JapaneseNumber> {
-  return [];
+const FIRST_TEN_NUMBERS: ReadonlyArray<ReadonlyArray<JapaneseNumber>> = [
+  [
+    {
+      kana: "ゼロ",
+      kanji: null
+    },
+    {
+      kana: "れい",
+      kanji: null
+    }
+  ],
+  [
+    {
+      kana: "いち",
+      kanji: "一"
+    }
+  ],
+  [
+    {
+      kana: "に",
+      kanji: "二"
+    }
+  ],
+  [
+    {
+      kana: "さん",
+      kanji: "三"
+    }
+  ],
+  [
+    {
+      kana: "よん",
+      kanji: "四"
+    },
+    {
+      kana: "し",
+      kanji: "四"
+    }
+  ],
+  [
+    {
+      kana: "ご",
+      kanji: "五"
+    }
+  ],
+  [
+    {
+      kana: "ろく",
+      kanji: "六"
+    }
+  ],
+  [
+    {
+      kana: "しち",
+      kanji: "七"
+    },
+    {
+      kana: "なな",
+      kanji: "七"
+    }
+  ],
+  [
+    {
+      kana: "はち",
+      kanji: "八"
+    }
+  ],
+  [
+    {
+      kana: "きゅう",
+      kanji: "九"
+    }
+  ]
+];
+
+const OKU_NUMBER: ReadonlyArray<JapaneseNumber> = [
+  {
+    kana: "おく",
+    kanji: "億"
+  }
+];
+
+const MAN_NUMBER: ReadonlyArray<JapaneseNumber> = [
+  {
+    kana: "まん",
+    kanji: "万"
+  }
+];
+
+const SEN_NUMBER: ReadonlyArray<JapaneseNumber> = [
+  {
+    kana: "せん",
+    kanji: "千"
+  }
+];
+
+const HYAKU_NUMBER: ReadonlyArray<JapaneseNumber> = [
+  {
+    kana: "ひゃく",
+    kanji: "百"
+  }
+];
+
+const JYUU_NUMBER: ReadonlyArray<JapaneseNumber> = [
+  {
+    kana: "じゅう",
+    kanji: "十"
+  }
+];
+
+function japaneseNumberCombiner(
+  first: JapaneseNumber,
+  second: JapaneseNumber
+): JapaneseNumber {
+  return {
+    kana: first.kana + second.kana,
+    kanji: first.kanji && second.kanji ? first.kanji + second.kanji : null
+  };
 }
+
+function japaneseNumberIteratee(num: JapaneseNumber): string {
+  return num.kana + "-" + num.kanji;
+}
+
+function conjugate(amount: number): ReadonlyArray<JapaneseNumber> {
+  if (amount < JYUU) {
+    return FIRST_TEN_NUMBERS[amount];
+  }
+
+  const breakdown = breakDownNumber(amount);
+  const chunks: Array<ReadonlyArray<JapaneseNumber>> = [];
+
+  if (breakdown.oku) {
+    if (breakdown.oku > 1) {
+      chunks.push(conjugateNumber(breakdown.oku));
+    }
+
+    chunks.push(OKU_NUMBER);
+  }
+
+  if (breakdown.man) {
+    if (breakdown.man > 1) {
+      chunks.push(conjugateNumber(breakdown.man));
+    }
+
+    chunks.push(MAN_NUMBER);
+  }
+
+  if (breakdown.sen) {
+    if (breakdown.sen > 1) {
+      chunks.push(conjugateNumber(breakdown.sen));
+    }
+
+    chunks.push(SEN_NUMBER);
+  }
+
+  if (breakdown.hyaku) {
+    if (breakdown.hyaku > 1) {
+      chunks.push(conjugateNumber(breakdown.hyaku));
+    }
+
+    chunks.push(HYAKU_NUMBER);
+  }
+
+  if (breakdown.jyuu) {
+    if (breakdown.jyuu > 1) {
+      chunks.push(conjugateNumber(breakdown.jyuu));
+    }
+
+    chunks.push(JYUU_NUMBER);
+  }
+
+  if (breakdown.solo) {
+    chunks.push(conjugateNumber(breakdown.solo));
+  }
+
+  return uniqBy(
+    permutate(chunks, japaneseNumberCombiner),
+    japaneseNumberIteratee
+  );
+}
+
+export const conjugateNumber: (
+  amount: number
+) => ReadonlyArray<JapaneseNumber> = memoize(conjugate);
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,50 @@
 export function randomFromArray<T>(arr: ReadonlyArray<T>): T {
   return arr[Math.floor(Math.random() * arr.length)];
 }
+
+// from [["a", "b"], ["c", "d"]], get ["ac", "ad", "bc", "bd"]
+export function permutate<T>(
+  arrays: ReadonlyArray<ReadonlyArray<T>>,
+  combiner: (first: T, second: T) => T
+): ReadonlyArray<T> {
+  if (arrays.length === 0) {
+    return [];
+  }
+
+  if (arrays.length === 1) {
+    return arrays[0];
+  }
+
+  if (arrays.length === 2) {
+    return permutateTwoArrays(arrays[0], arrays[1], combiner);
+  }
+
+  return permutateTwoArrays(
+    arrays[0],
+    permutate(arrays.slice(1), combiner),
+    combiner
+  );
+}
+
+function permutateTwoArrays<T>(
+  first: ReadonlyArray<T>,
+  second: ReadonlyArray<T>,
+  combiner: (first: T, second: T) => T
+): ReadonlyArray<T> {
+  if (!first.length) {
+    return second;
+  }
+
+  if (!second.length) {
+    return first;
+  }
+
+  const permutations: T[] = [];
+  for (const a of first) {
+    for (const b of second) {
+      permutations.push(combiner(a, b));
+    }
+  }
+
+  return permutations;
+}
