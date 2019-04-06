14d4e1db247478c242f2beddcc093dedb6c7409e
Alec Deitloff <alec.deitloff@gmail.com>
2019-04-06 13:08:28 -0700

    split out webpack files to prevent need to download unchanged files

--- webpack.common.js
+++ webpack.common.js
@@ -5,14 +5,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ROOT_DIRECTORY = __dirname;
 const BUILD_DIRECTORY = path.resolve(ROOT_DIRECTORY, "build");
 const SOURCE_DIRECTORY = path.resolve(ROOT_DIRECTORY, "src");
+const DATA_DIRECTORY = path.resolve(SOURCE_DIRECTORY, "data");
 
 module.exports = {
   entry: {
-    app: [SOURCE_DIRECTORY]
+    app: SOURCE_DIRECTORY,
+    data: [
+      path.resolve(DATA_DIRECTORY, "counters.ts"),
+      path.resolve(DATA_DIRECTORY, "items.ts"),
+      path.resolve(DATA_DIRECTORY, "study-packs")
+    ]
   },
   output: {
     path: BUILD_DIRECTORY,
-    filename: "[name].js"
+    filename: "[name].[contenthash].js"
   },
   resolve: {
     extensions: [".js", ".jsx", ".json", ".ts", ".tsx", ".scss"]
@@ -47,7 +53,27 @@ module.exports = {
       }
     ]
   },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\]/,
+          name: function(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return packageName;
+          }
+        }
+      }
+    }
+  },
   plugins: [
+    new webpack.HashedModuleIdsPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(SOURCE_DIRECTORY, "index.html")
     })
