336aa7a50dacd88aab010e45c1d2c3b62cc66484
Alec Deitloff <alec.deitloff@gmail.com>
2019-06-02 23:16:29 -0700

    add export of items file

---
+++ data/items.ts
@@ -0,0 +1,26 @@
+import { Item } from "../src/redux";
+
+const ITEM_DAY_OF_THE_MONTH: Item = {
+  counters: ["days of the month"],
+  englishPlural: "days of the month",
+  englishSingular: "day of the month",
+  itemId: "day of the month",
+  maxQuantity: 31,
+  minQuantity: 1
+};
+
+const ITEM_MINUTE: Item = {
+  counters: ["minute"],
+  englishPlural: "minutes",
+  englishSingular: "minute",
+  itemId: "minute",
+  maxQuantity: 100,
+  minQuantity: 1
+};
+
+export const ITEMS_FROM_COUNTER: {
+  [counterId: string]: ReadonlyArray<Item>;
+} = {
+  "days of the month": [ITEM_DAY_OF_THE_MONTH],
+  minute: [ITEM_MINUTE]
+};
--- scripts/build-data.js
+++ scripts/build-data.js
@@ -6,14 +6,23 @@ const ROOT_DIRECTORY = path.resolve(__dirname, "..");
 const DATABASE_FILE = path.resolve(ROOT_DIRECTORY, "jyosuushi.sqlite");
 const DATA_DIRECTORY = path.resolve(ROOT_DIRECTORY, "data");
 const COUNTERS_FILE = path.resolve(DATA_DIRECTORY, "counters.ts");
+const ITEMS_FILE = path.resolve(DATA_DIRECTORY, "items.ts");
 
 function getVariableFromId(prefix, id) {
   return prefix + id.toUpperCase().replace(/[^A-Z0-9]+/g, "_");
 }
 
+function getCounterId(id) {
+  return getVariableFromId("COUNTER_", id);
+}
+
+function getItemId(id) {
+  return getVariableFromId("ITEM_", id);
+}
+
 // Counters
 async function writeCounterData(file, counter, irregulars) {
-  const variableName = getVariableFromId("COUNTER_", counter.counter_id);
+  const variableName = getCounterId(counter.counter_id);
   let irregularsStr;
   if (irregulars && Object.keys(irregulars).length) {
     irregularsStr = "{\n";
@@ -71,9 +80,98 @@ async function writeCountersFile(db) {
   fs.closeSync(file);
 }
 
+// Items
+async function writeItemData(file, item, counters) {
+  const variableName = getItemId(item.item_id);
+  let countersStr = "[\n";
+  for (let index = 0; index < counters.length; ++index) {
+    countersStr += `  "${counters[index]}"`;
+
+    if (index < counters.length - 1) {
+      countersStr += ",\n";
+    }
+  }
+
+  countersStr += "\n  ]";
+
+  fs.writeSync(
+    file,
+    `\n\nconst ${variableName}: Item = {
+  counters: ${JSON.stringify(counters)},
+  englishPlural: "${item.english_plural}",
+  englishSingular: "${item.english_singular}",
+  itemId: "${item.item_id}",
+  maxQuantity: ${item.custom_max_amount || 100},
+  minQuantity: ${item.custom_min_amount || 1}
+};`
+  );
+}
+
+async function writeItemsFile(db) {
+  const items = await db.all("SELECT * FROM items ORDER BY item_id ASC");
+  const itemCounters = await db.all("SELECT * FROM item_counters");
+  const itemsToCounters = {};
+  const countersToItems = {};
+  for (const itemCounter of itemCounters) {
+    if (!itemsToCounters[itemCounter.item_id]) {
+      itemsToCounters[itemCounter.item_id] = [];
+    }
+
+    itemsToCounters[itemCounter.item_id].push(itemCounter.counter_id);
+
+    if (!countersToItems[itemCounter.counter_id]) {
+      countersToItems[itemCounter.counter_id] = [];
+    }
+
+    countersToItems[itemCounter.counter_id].push(itemCounter.item_id);
+  }
+
+  const file = fs.openSync(ITEMS_FILE, "w");
+
+  fs.writeSync(file, 'import { Item } from "../src/redux";');
+
+  for (const item of items) {
+    if (!itemsToCounters[item.item_id]) {
+      continue;
+    }
+
+    writeItemData(file, item, itemsToCounters[item.item_id]);
+  }
+
+  fs.writeSync(file, "\n\nexport const ITEMS_FROM_COUNTER: {\n");
+  fs.writeSync(file, "  [counterId: string]: ReadonlyArray<Item>;\n");
+  fs.writeSync(file, "} = {\n");
+  let hasWrittenFirst = false;
+  for (const counterId in countersToItems) {
+    if (hasWrittenFirst) {
+      fs.writeSync(file, ",\n");
+    } else {
+      hasWrittenFirst = true;
+    }
+
+    let entryKey;
+    if (/^[a-zA-Z0-9]*$/.test(counterId)) {
+      entryKey = counterId;
+    } else {
+      entryKey = `"${counterId}"`;
+    }
+
+    fs.writeSync(
+      file,
+      `  ${entryKey}: [${countersToItems[counterId]
+        .map(itemId => getItemId(itemId))
+        .join(", ")}]`
+    );
+  }
+
+  fs.writeSync(file, "\n};\n");
+  fs.closeSync(file);
+}
+
 async function main() {
   const db = await sqlite.open(DATABASE_FILE, { Promise });
   await writeCountersFile(db);
+  await writeItemsFile(db);
   await db.close();
 }
 
