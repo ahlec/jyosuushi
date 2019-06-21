451fe7246550d6c0129fe2ab965d251d83c4c036
Alec Jacob Deitloff <alec.deitloff@gmail.com>
2019-06-21 00:25:00 -0700

    improve the beta explanation message

--- src/ui/intro-page/BetaExplanation.tsx
+++ src/ui/intro-page/BetaExplanation.tsx
@@ -9,17 +9,17 @@ export default class BetaExplanation extends React.PureComponent {
   public render() {
     return (
       <div className="BetaExplanation">
-        We're in open beta right now! That means that we hope you enjoy the
-        application and give us{" "}
+        We're in open beta right now! I hope you enjoy the application and give
+        me{" "}
         <a href={CONFIG_FEEDBACK_FORM_LINK} target="_blank">
           <img src={CommentsIcon} /> <span>your feedback and ideas</span>
         </a>{" "}
-        for improving it! But also, please expect the unfortunate bug, and use
-        the{" "}
+        for improving it! But also, please expect the unfortunate bug or
+        mistake, and use the{" "}
         <a href={CONFIG_BUG_REPORT_FORM_LINK} target="_blank">
           <img src={BugIcon} /> <span>bug report</span>
         </a>{" "}
-        link to let us know! Also, expect updates frequently!
+        link to let me know! Expect updates frequently!
       </div>
     );
   }
