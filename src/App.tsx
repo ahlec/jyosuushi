20353485766bdc77abbad0b7db6cf612f46637bb
Alec Jacob Deitloff <alec.deitloff@gmail.com>
2019-07-02 22:58:08 -0700

    transition between quiz and main screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import classnames from "classnames";
 import * as React from "react";
 import { connect } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import {
+  Redirect,
+  Route,
+  RouteComponentProps,
+  Switch,
+  withRouter
+} from "react-router-dom";
 
 import { State } from "./redux";
 
@@ -10,6 +16,8 @@ import MainScreen from "./ui/main-screen/MainScreen";
 import QuizPage from "./ui/quiz-page";
 
 import "./App.scss";
+
+const QUIZ_SCREEN_PATH = "/quiz";
 
 interface ReduxProps {
   isQuizActive: boolean;
@@ -25,26 +33,45 @@ interface ComponentState {
   isModalOpen: boolean;
 }
 
-class App extends React.PureComponent<ReduxProps, ComponentState> {
+type ComponentProps = ReduxProps & RouteComponentProps;
+
+class App extends React.PureComponent<ComponentProps, ComponentState> {
   public state: ComponentState = {
     isModalOpen: false
   };
 
   public render() {
     const { isQuizActive } = this.props;
+
     return (
       <div className={classnames("App", isQuizActive && "quiz-active")}>
         <Header
           isQuizActive={isQuizActive}
           onModalOpened={this.onHeaderModalOpened}
         />
+        {this.renderNecessaryRedirect()}
         <Switch>
-          <Route path="/quiz" render={this.renderQuizPage} />
+          <Route path={QUIZ_SCREEN_PATH} render={this.renderQuizPage} />
           <Route render={this.renderMainScreen} />
         </Switch>
       </div>
     );
   }
+
+  private renderNecessaryRedirect = () => {
+    const {
+      location: { pathname },
+      isQuizActive
+    } = this.props;
+    const isOnQuizScreen = pathname === QUIZ_SCREEN_PATH;
+    if (isOnQuizScreen && !isQuizActive) {
+      return <Redirect to="/" />;
+    }
+
+    if (!isOnQuizScreen && isQuizActive) {
+      return <Redirect to={QUIZ_SCREEN_PATH} />;
+    }
+  };
 
   private renderMainScreen = () => {
     return <MainScreen />;
@@ -59,4 +86,4 @@ class App extends React.PureComponent<ReduxProps, ComponentState> {
     this.setState({ isModalOpen });
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(withRouter(App));
