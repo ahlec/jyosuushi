939eb9e0a72bc9c7fa359112dcfb60c52491584e
Alec Deitloff <alec.deitloff@gmail.com>
2020-04-12 16:17:23 -0700

    Remove broken conjugation section of counter page (WIP)

--- src/ui/main-screen/explore/counter/ConjugationsSection.tsx
+++ src/ui/main-screen/explore/counter/ConjugationsSection.tsx
@@ -1,10 +1,9 @@
-import classnames from "classnames";
-import { clamp, memoize } from "lodash";
+import { clamp } from "lodash";
 import * as React from "react";
 
-import { ConjugationCategory, Counter } from "@jyosuushi/interfaces";
+import { Conjugation, Counter } from "@jyosuushi/interfaces";
 import Localization from "@jyosuushi/localization";
-import { conjugateCounter, ConjugatedInfo } from "@jyosuushi/utils";
+import { conjugateCounter } from "@jyosuushi/japanese/counters";
 
 import "./ConjugationsSection.scss";
 
@@ -12,75 +11,75 @@ const AMOUNTS_TO_DISPLAY = 17;
 const MIN_USER_INPUT = 1;
 const MAX_USER_INPUT = 999_999_999;
 
-interface Irregular {
-  conjugation: string;
-  amount: number;
-}
+// interface Irregular {
+//   conjugation: string;
+//   amount: number;
+// }
 
-const getConjugations = memoize(
-  (counter: Counter): ReadonlyArray<ReadonlyArray<ConjugatedInfo>> => {
-    const results: Array<ReadonlyArray<ConjugatedInfo>> = [];
-    for (let amount = 1; amount <= AMOUNTS_TO_DISPLAY; ++amount) {
-      results[amount - 1] = conjugateCounter(amount, counter);
-    }
+// const getConjugations = memoize(
+//   (counter: Counter): ReadonlyArray<ReadonlyArray<ConjugatedInfo>> => {
+//     const results: Array<ReadonlyArray<ConjugatedInfo>> = [];
+//     for (let amount = 1; amount <= AMOUNTS_TO_DISPLAY; ++amount) {
+//       results[amount - 1] = conjugateCounter(amount, counter);
+//     }
 
-    return results;
-  },
-  (counter: Counter) => counter.counterId
-);
+//     return results;
+//   },
+//   (counter: Counter) => counter.counterId
+// );
 
-const countIrregulars = memoize(
-  (conjugations: ReadonlyArray<ReadonlyArray<ConjugatedInfo>>) =>
-    conjugations.reduce(
-      (total: number, nested: ReadonlyArray<ConjugatedInfo>) => {
-        return (
-          total +
-          nested.filter(
-            ({ category }) => category === ConjugationCategory.Irregular
-          ).length
-        );
-      },
-      0
-    )
-);
+// const countIrregulars = memoize(
+//   (conjugations: ReadonlyArray<ReadonlyArray<ConjugatedInfo>>) =>
+//     conjugations.reduce(
+//       (total: number, nested: ReadonlyArray<ConjugatedInfo>) => {
+//         return (
+//           total +
+//           nested.filter(
+//             ({ category }) => category === ConjugationCategory.Irregular
+//           ).length
+//         );
+//       },
+//       0
+//     )
+// );
 
-function compareIrregulars(a: Irregular, b: Irregular): number {
-  return a.amount - b.amount;
-}
+// function compareIrregulars(a: Irregular, b: Irregular): number {
+//   return a.amount - b.amount;
+// }
 
-const getFurtherIrregulars = memoize(
-  (counter: Counter): ReadonlyArray<Irregular> => {
-    const results: Irregular[] = [];
-    Object.keys(counter.irregulars).forEach(amountStr => {
-      const amount = parseInt(amountStr, 10);
-      if (amount <= AMOUNTS_TO_DISPLAY) {
-        return;
-      }
+// const getFurtherIrregulars = memoize(
+//   (counter: Counter): ReadonlyArray<Irregular> => {
+//     const results: Irregular[] = [];
+//     Object.keys(counter.irregulars).forEach(amountStr => {
+//       const amount = parseInt(amountStr, 10);
+//       if (amount <= AMOUNTS_TO_DISPLAY) {
+//         return;
+//       }
 
-      // TODO: This won't group multiple irregulars of the same amount so
-      // they'll appear on different lines.
-      const conjugations = conjugateCounter(amount, counter);
-      for (const conjugation of conjugations) {
-        results.push({
-          amount,
-          conjugation: conjugation.kana
-        });
-      }
-    });
+//       // TODO: This won't group multiple irregulars of the same amount so
+//       // they'll appear on different lines.
+//       const conjugations = conjugateCounter(amount, counter);
+//       for (const conjugation of conjugations) {
+//         results.push({
+//           amount,
+//           conjugation: conjugation.kana
+//         });
+//       }
+//     });
 
-    results.sort(compareIrregulars);
-    return results;
-  },
-  (counter: Counter) => counter.counterId
-);
+//     results.sort(compareIrregulars);
+//     return results;
+//   },
+//   (counter: Counter) => counter.counterId
+// );
 
-function highlightIrregular(contents: string): React.ReactNode {
-  return (
-    <span className="irregular" key={contents}>
-      {contents}
-    </span>
-  );
-}
+// function highlightIrregular(contents: string): React.ReactNode {
+//   return (
+//     <span className="irregular" key={contents}>
+//       {contents}
+//     </span>
+//   );
+// }
 
 interface ComponentProps {
   counter: Counter;
@@ -103,12 +102,12 @@ export default class ConjugationsSection extends React.PureComponent<
     const { counter, localization } = this.props;
     const { currentUserInput } = this.state;
 
-    const conjugations = getConjugations(counter);
-    const furtherIrregulars = getFurtherIrregulars(counter);
+    // const conjugations = getConjugations(counter);
+    // const furtherIrregulars = getFurtherIrregulars(counter);
 
     return (
       <section className="ConjugationsSection">
-        <p className="examples-prefix">
+        {/* <p className="examples-prefix">
           {localization.hereAreTheFirstXNumbers(AMOUNTS_TO_DISPLAY)}{" "}
           {this.renderIrregularsWarning(counter)}
         </p>
@@ -125,7 +124,7 @@ export default class ConjugationsSection extends React.PureComponent<
               {furtherIrregulars.map(this.renderFurtherIrregular)}
             </div>
           </React.Fragment>
-        )}
+        )} */}
         <p className="custom-input-prefix">
           {localization.customCounterAmountInputPrefix}
         </p>
@@ -146,81 +145,71 @@ export default class ConjugationsSection extends React.PureComponent<
     );
   }
 
-  private renderIrregularsWarning(counter: Counter): React.ReactNode {
-    const {
-      props: { localization }
-    } = this;
-    const conjugations = getConjugations(counter);
-    const furtherIrregulars = getFurtherIrregulars(counter);
-    const numIrregulars =
-      countIrregulars(conjugations) + furtherIrregulars.length;
-    if (!numIrregulars) {
-      return localization.irregularsWarningNoIrregulars;
-    }
+  // private renderIrregularsWarning(counter: Counter): React.ReactNode {
+  //   const {
+  //     props: { localization }
+  //   } = this;
+  //   const conjugations = getConjugations(counter);
+  //   const furtherIrregulars = getFurtherIrregulars(counter);
+  //   const numIrregulars =
+  //     countIrregulars(conjugations) + furtherIrregulars.length;
+  //   if (!numIrregulars) {
+  //     return localization.irregularsWarningNoIrregulars;
+  //   }
 
-    return localization.irregularsWarning(numIrregulars, highlightIrregular);
-  }
+  //   return localization.irregularsWarning(numIrregulars, highlightIrregular);
+  // }
 
-  private renderAmountTile = (
-    conjugations: ReadonlyArray<ConjugatedInfo>,
-    index: number
-  ): React.ReactNode => {
-    const amount = index + 1;
-    return (
-      <div className="conjugation-container" key={index}>
-        <div className="amount">{amount}</div>
-        <div className="conjugations">
-          {conjugations.map(this.renderConjugation)}
-        </div>
-      </div>
-    );
-  };
+  // private renderAmountTile = (
+  //   conjugations: ReadonlyArray<Conjugation>,
+  //   index: number
+  // ): React.ReactNode => {
+  //   const amount = index + 1;
+  //   return (
+  //     <div className="conjugation-container" key={index}>
+  //       <div className="amount">{amount}</div>
+  //       <div className="conjugations">
+  //         {conjugations.map(this.renderConjugation)}
+  //       </div>
+  //     </div>
+  //   );
+  // };
 
-  private renderFurtherIrregular = ({
-    amount,
-    conjugation
-  }: Irregular): React.ReactNode => {
-    return (
-      <div className="conjugation-container" key={amount}>
-        <div className="amount">{amount}</div>
-        <div className="conjugations">
-          <div className="irregular">{conjugation}</div>
-        </div>
-      </div>
-    );
-  };
+  // private renderFurtherIrregular = ({
+  //   amount,
+  //   conjugation
+  // }: Irregular): React.ReactNode => {
+  //   return (
+  //     <div className="conjugation-container" key={amount}>
+  //       <div className="amount">{amount}</div>
+  //       <div className="conjugations">
+  //         <div className="irregular">{conjugation}</div>
+  //       </div>
+  //     </div>
+  //   );
+  // };
 
   private renderCurrentUserInputItem = (
-    { category, kana }: ConjugatedInfo,
+    conjugation: Conjugation,
     index: number
-  ): React.ReactNode => (
-    <div
-      key={index}
-      className={classnames(
-        category === ConjugationCategory.Irregular && "irregular",
-        category === ConjugationCategory.Strange && "strange"
-      )}
-    >
-      {kana}
-    </div>
-  );
+  ): React.ReactNode => <div key={index}>{conjugation.reading}</div>;
 
-  private renderConjugation = ({
-    category,
-    kana
-  }: ConjugatedInfo): React.ReactNode => {
-    return (
-      <div
-        key={kana}
-        className={classnames(
-          category === ConjugationCategory.Strange && "strange",
-          category === ConjugationCategory.Irregular && "irregular"
-        )}
-      >
-        {kana}
-      </div>
-    );
-  };
+  // private renderConjugation = ({
+  //   category,
+  //   kana
+  // }: Conjugation): React.ReactNode => {
+  //   return (
+  //     <div
+  //       key={kana}
+  //       className={classnames(
+  //         category === ConjugationCategory.Strange && "strange",
+  //         category === ConjugationCategory.Irregular && "irregular"
+  //       )}
+  //     >
+  //       {kana}
+  //     </div>
+  //   );
+  // };
 
   private onUserInputChanged = (
     event: React.ChangeEvent<HTMLInputElement>
