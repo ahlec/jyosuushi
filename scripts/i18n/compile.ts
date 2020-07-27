3d848bca85e1ca26630c27bfca0436c52afbc111
Alec Deitloff <alec.deitloff@gmail.com>
2020-07-26 17:15:40 -0700

    JSS-14: Build custom script to compile translation files --> runtime

---
+++ scripts/i18n/compile.ts
@@ -0,0 +1,32 @@
+import { parse, MessageFormatElement } from "intl-messageformat-parser";
+import { outputJSONSync } from "fs-extra";
+
+import LocaleFile from "./LocaleFile";
+
+async function compileFile(inputFile: string, outFile: string): Promise<void> {
+  const { entries } = await LocaleFile.load(inputFile);
+
+  const results: Record<string, string | MessageFormatElement[]> = {};
+  for (const [
+    id,
+    {
+      original: { defaultMessage },
+      translation,
+    },
+  ] of entries) {
+    results[id] = parse(translation || defaultMessage);
+  }
+
+  outputJSONSync(outFile, results, {
+    spaces: 2,
+  });
+}
+
+async function main(): Promise<void> {
+  await Promise.all([
+    compileFile("./i18n/en.json", "src/client/i18n/translations/en.json"),
+    compileFile("./i18n/jp.json", "src/client/i18n/translations/jp.json"),
+  ]);
+}
+
+main();
