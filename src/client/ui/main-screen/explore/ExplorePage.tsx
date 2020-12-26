472f49ace217531f3947ff89dd9badeb7d467be3
Alec Deitloff <alec.deitloff@gmail.com>
2020-12-26 16:01:21 -0600

    Switch ExplorePage to be a functional component

--- src/client/ui/main-screen/explore/ExplorePage.tsx
+++ src/client/ui/main-screen/explore/ExplorePage.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useCallback } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import { EXPLORE_COUNTER_PATH, EXPLORE_STUDY_PACK_PATH } from "./pathing";
@@ -11,22 +11,10 @@ import ExploreStudyPackPage from "./study-pack/ExploreStudyPackPage";
 
 import styles from "./ExplorePage.scss";
 
-export default class ExplorePage extends React.PureComponent {
-  public render(): React.ReactNode {
-    return (
-      <Switch>
-        <Route
-          path={EXPLORE_STUDY_PACK_PATH}
-          component={ExploreStudyPackPage}
-        />
-        <Route path={EXPLORE_COUNTER_PATH} component={ExploreCounterPage} />
-        <Route render={this.renderLandingPage} />
-      </Switch>
-    );
-  }
-
-  private renderLandingPage = (): React.ReactNode => {
-    return (
+function ExplorePage(): React.ReactElement {
+  // Create a memoized function to render the fallback, main page
+  const renderLandingPage = useCallback(
+    (): React.ReactElement => (
       <div className={styles.explorePageLanding}>
         <BreadcrumbBar />
         <div className={styles.contents}>
@@ -34,6 +22,18 @@ export default class ExplorePage extends React.PureComponent {
           <AllCounters />
         </div>
       </div>
-    );
-  };
+    ),
+    []
+  );
+
+  // Render the component
+  return (
+    <Switch>
+      <Route path={EXPLORE_STUDY_PACK_PATH} component={ExploreStudyPackPage} />
+      <Route path={EXPLORE_COUNTER_PATH} component={ExploreCounterPage} />
+      <Route render={renderLandingPage} />
+    </Switch>
+  );
 }
+
+export default ExplorePage;
