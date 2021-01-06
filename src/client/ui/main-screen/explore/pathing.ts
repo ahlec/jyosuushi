38e083ed1778208dccfbfd37ce9301c3a12e42d9
Alec Deitloff <alec.deitloff@gmail.com>
2021-01-05 20:35:25 -0800

    Rename collection URL pathing from 'study-pack'  to 'collection'

--- src/client/ui/main-screen/explore/pathing.ts
+++ src/client/ui/main-screen/explore/pathing.ts
@@ -1,13 +1,10 @@
 export const EXPLORE_PAGE_PATH = "/explore";
 
-const EXPLORE_STUDY_PACK_PATH_ROOT = `${EXPLORE_PAGE_PATH}/study-pack`;
-export const EXPLORE_STUDY_PACK_PATH = `${EXPLORE_STUDY_PACK_PATH_ROOT}/:packId`;
-
 const EXPLORE_COUNTER_PATH_ROOT = `${EXPLORE_PAGE_PATH}/counter`;
 export const EXPLORE_COUNTER_PATH = `${EXPLORE_COUNTER_PATH_ROOT}/:counterId`;
 
 export function getCounterCollectionPath(id: string): string {
-  return `${EXPLORE_STUDY_PACK_PATH_ROOT}/${id}`;
+  return `${EXPLORE_PAGE_PATH}/collection/${id}`;
 }
 
 export function getCounterLink(id: string): string {
