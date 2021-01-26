44812e8642bf181e5302a8b0d26fef24be32cb06
Alec Deitloff <alec.deitloff@gmail.com>
2021-01-25 21:12:19 -0800

    Build rename collection modal and hook it up to GraphQL

--- src/client/ui/main-screen/explore/collections/CounterCollectionView.tsx
+++ src/client/ui/main-screen/explore/collections/CounterCollectionView.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 import { CounterCollection } from "@jyosuushi/graphql/types.generated";
 
@@ -9,6 +9,7 @@ import { getCounterCollectionPath } from "@jyosuushi/ui/main-screen/explore/path
 
 import CollectionHeader from "./CollectionHeader";
 import EntriesSection from "./entries-section/EntriesSection";
+import RenameCollectionModal from "./rename-collection-modal/RenameCollectionModal";
 
 import styles from "./CounterCollectionView.scss";
 
@@ -19,6 +20,9 @@ interface ComponentProps {
 function CounterCollectionView({
   collection,
 }: ComponentProps): React.ReactElement {
+  // Define component state
+  const [isRenameModalOpen, setIsRenameModalOpen] = useState<boolean>(false);
+
   // Prepare the links to appear in the breadcrumb bar
   const breadcrumbLinks = useMemo(
     (): readonly BreadcrumbBarLinkDefinition[] => [
@@ -31,6 +35,17 @@ function CounterCollectionView({
     [collection.id, collection.name]
   );
 
+  // Handle events
+  const handleOpenRenameModal = useCallback(
+    (): void => setIsRenameModalOpen(true),
+    []
+  );
+
+  const handleRequestCloseRenameModal = useCallback(
+    (): void => setIsRenameModalOpen(false),
+    []
+  );
+
   // Render the component
   return (
     <div className={styles.counterCollectionView}>
@@ -38,9 +53,16 @@ function CounterCollectionView({
         <BreadcrumbBar links={breadcrumbLinks} />
       </div>
       <CollectionHeader collection={collection} />
+      <button onClick={handleOpenRenameModal}>Rename</button>
       <div className={styles.pageArea}>
         <EntriesSection collection={collection} />
       </div>
+      {isRenameModalOpen && (
+        <RenameCollectionModal
+          collectionId={collection.id}
+          onRequestClose={handleRequestCloseRenameModal}
+        />
+      )}
     </div>
   );
 }
